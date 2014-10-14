5f8456525a223b67b607f1b057214d355c9159f3
Christopher R. Palmer <crpalmer@gmail.com>
2014-10-14 06:51:14 -0400

    tobacco-spitter: Add a remote API

--- 2014/tobacco-spitter.c
+++ 2014/tobacco-spitter.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
 #include <time.h>
+#include <unistd.h>
 #include "piface.h"
+#include "server.h"
 #include "track.h"
 #include "util.h"
 
@@ -19,6 +22,7 @@
 
 static piface_t *p;
 static unsigned n_consecutive_spits = 0;
+static pthread_mutex_t event_lock;
 
 static track_t *burp_track;
 
@@ -65,13 +69,14 @@ spit()
     ms_sleep(PUMP_MS);
     piface_set(p, PUMP_OUTPUT, false);
 
-
     n_consecutive_spits++;
 }
 
 static void
 take_action()
 {
+    pthread_mutex_lock(&event_lock);
+
     if (randomly_with_prob(BURP_PROB)) {
 	if (! n_consecutive_spits) {
 	    printf("Too many burps, do a spit instead.\n");
@@ -87,11 +92,43 @@ take_action()
 	    spit();
 	}
     }
+
+    pthread_mutex_unlock(&event_lock);
 }
+
+static char *
+remote_event_locked(const char *command)
+{
+    if (strcmp(command, "burp") == 0) {
+	burp();
+    } else if (strcmp(command, "spit") == 0) {
+	spit();
+    } else {
+	return strdup("Invalid command");
+    }
+
+    return strdup(SERVER_OK);
+}
+
+static char *
+remote_event(void *unused, const char *command)
+{
+    char *response;
+
+    pthread_mutex_lock(&event_lock);
+    response = remote_event_locked(command);
+    pthread_mutex_unlock(&event_lock);
+
+    return response;
+}
+
 
 int
 main(int argc, char **argv)
 {
+    server_args_t server_args;
+    pthread_t server_thread;
+
     seed_random();
 
     burp_track = track_new(BURP_WAV);
@@ -101,6 +138,13 @@ main(int argc, char **argv)
     }
 
     p = piface_new();
+
+    pthread_mutex_init(&event_lock, NULL);
+    server_args.port = 5555;
+    server_args.command = remote_event;
+    server_args.state = NULL;
+
+    pthread_create(&server_thread, NULL, server_thread_main, &server_args);
 
     while(true) {
 	wait_for_trigger();
