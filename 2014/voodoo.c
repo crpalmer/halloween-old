ca85ec1d38a590bfd82cb82c79c60ee52333b2ff
Christopher R. Palmer <crpalmer@gmail.com>
2014-10-15 04:55:05 -0400

    voodoo: Add a remote API

--- 2014/voodoo.c
+++ 2014/voodoo.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
+#include <pthread.h>
 #include "audio.h"
 #include "maestro.h"
 #include "mem.h"
 #include "pi-usb.h"
 #include "piface.h"
+#include "server.h"
 #include "talking-skull.h"
 
 static maestro_t *maestro;
@@ -24,6 +27,10 @@ static double history[N_HISTORY];
 static size_t history_i;
 static double sum_history;
 static bool history_full;
+
+static pthread_mutex_t speak_lock;
+static audio_t *out;
+static talking_skull_t *skull;
 
 static double gain = 10;
 
@@ -52,16 +59,62 @@ servo_update(void *unused, double pos)
     piface_set(piface, EYES, pos >= 50);
 }
 
+static void
+play_one_buffer(void *buffer, size_t size)
+{
+    pthread_mutex_lock(&speak_lock);
+    talking_skull_play(skull, buffer, size);
+    if (! audio_play_buffer(out, buffer, size)) {
+	fprintf(stderr, "Failed to play buffer!\n");
+	exit(1);
+    }
+    pthread_mutex_unlock(&speak_lock);
+}
+
+static char *
+remote_event(void *unused, const char *command)
+{
+    unsigned char *data;
+    size_t i, j;
+
+fprintf(stderr, "remote: [%s]\n", command);
+    if (strncmp(command, "play ", 5) != 0) {
+	return strdup("Invalid command");
+    }
+
+    data = malloc(strlen(command));
+    for (i = 5; command[i]; i++, j++) {
+	if (command[i] == '\\') {
+	    switch(command[++i]) {
+	    case '\\': data[j] = '\\'; break;
+	    case 'n': data[j] = '\n'; break;
+	    case '0': data[j] = '\0'; break;
+	    default:
+		free(data);
+		return strdup("corrupt data");
+	    }
+	} else {
+	    data[j] = data[i];
+	}
+    }
+
+    play_one_buffer(data, j);
+    free(data);
+
+    return strdup(SERVER_OK);
+}
+
 int
 main(int argc, char **argv)
 {
     unsigned char *buffer;
     size_t size;
-    audio_t *in, *out;
+    audio_t *in;
     audio_config_t cfg;
     audio_meta_t meta;
     audio_device_t in_dev, out_dev;
-    talking_skull_t *skull;
+    server_args_t server_args;
+    pthread_t server_thread;
 
     pi_usb_init();
     if ((maestro = maestro_new()) == NULL) {
@@ -71,8 +124,14 @@ main(int argc, char **argv)
 	maestro_set_servo_speed(maestro, SERVO_ID, 0);
     }
 
-
     piface = piface_new();
+
+    pthread_mutex_init(&speak_lock, NULL);
+    server_args.port = 5555;
+    server_args.command = remote_event;
+    server_args.state = NULL;
+
+    pthread_create(&server_thread, NULL, server_thread_main, &server_args);
 
     audio_config_init_default(&cfg);
     audio_device_init_playback(&in_dev);
@@ -107,11 +166,7 @@ main(int argc, char **argv)
     fprintf(stderr, "Copying from capture to play using %u byte buffers\n", size);
 
     while (audio_capture_buffer(in, buffer)) {
-	talking_skull_play(skull, buffer, size);
-	if (! audio_play_buffer(out, buffer, size)) {
-	    fprintf(stderr, "Failed to play buffer!\n");
-	    exit(1);
-	}
+	play_one_buffer(buffer, size);
     }
 
     fprintf(stderr, "Failed to capture buffer!\n");
