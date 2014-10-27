7d832f95e5c51ab876bf41388275ebd522093174
Christopher R. Palmer <crpalmer@gmail.com>
2014-10-27 16:19:49 -0400

    swamp: Add a server component

--- 2014/swamp.c
+++ 2014/swamp.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <time.h>
+#include <pthread.h>
 #include "piface.h"
+#include "server.h"
 #include "track.h"
 #include "util.h"
 
 #define DUTY_FILENAME "swamp.duty"
 #define DEFAULT_DUTY_CYCLE .10
+#define DUTY_DELTA 0.01
+#define FOG_BURST_MS	5000
 
 #define FOG_OUTPUT	7
 #define OFF_DELAY	5000
 
 static piface_t *p;
 static double duty = .10;
+static pthread_mutex_t lock;
 
 static double
 read_duty_cycle(void)
@@ -52,23 +58,71 @@ write_duty_cycle(double x)
     return x;
 }
 
+static void
+do_fog(unsigned ms)
+{
+    pthread_mutex_lock(&lock);
+    piface_set(p, FOG_OUTPUT, 1);
+    fprintf(stderr, "sleeping for ON\n");
+    ms_sleep(ms);
+    piface_set(p, FOG_OUTPUT, 0);
+    fprintf(stderr, "sleeping for OFF_DELAY\n");
+    ms_sleep(OFF_DELAY);
+    pthread_mutex_unlock(&lock);
+}
+
+static char *
+return_duty(void)
+{
+    char foo[1000];
+
+    sprintf(foo, "ok duty is now %f\n", duty);
+    return strdup(foo);
+}
+
+static char *
+remote_event(void *unused, const char *command)
+{
+    if (strcmp(command, "duty_up") == 0) {
+	duty += DUTY_DELTA;
+	if (duty >= 1) duty = 1;
+	write_duty_cycle(duty);
+	return return_duty();
+    } else if (strcmp(command, "duty_down") == 0) {
+	duty -= DUTY_DELTA;
+	if (duty <= 0) duty = 0;
+	write_duty_cycle(duty);
+	return return_duty();
+    } else if (strcmp(command, "fog") == 0) {
+	do_fog(FOG_BURST_MS);
+    } else {
+        return strdup("invalid command");
+    }
+
+    return strdup("ok");
+}
+
 int
 main(int argc, char **argv)
 {
+    server_args_t server_args;
+    pthread_t server_thread;
+
     p = piface_new();
     duty = read_duty_cycle();
 
-    write_duty_cycle(duty);
+    pthread_mutex_init(&lock, NULL);
+
+    server_args.port = 5555;
+    server_args.command = remote_event;
+    server_args.state = NULL;
+
+    pthread_create(&server_thread, NULL, server_thread_main, &server_args);
 
     while(true) {
 	fprintf(stderr, "sleeping for OFF\n");
 	ms_sleep(5000 * (1 - duty));
-	piface_set(p, FOG_OUTPUT, 1);
-	fprintf(stderr, "sleeping for ON\n");
-	ms_sleep(5000 * (duty));
-	piface_set(p, FOG_OUTPUT, 0);
-	fprintf(stderr, "sleeping for OFF_DELAY\n");
-	ms_sleep(OFF_DELAY);
+	do_fog(5000 * duty);
     }
 
     return 0;
