9554e81f2e6c6053e18c1fc9444e27ca50ca13d1
Christopher R. Palmer <crpalmer@gmail.com>
2015-10-07 06:39:07 -0400

    2015: band: Add drum framework and switch to talking_skull prepare

--- 2015/band.c
+++ 2015/band.c
@@ -8,6 +8,8 @@
 #include "track.h"
 #include "wav.h"
 
+#define DRUM_LEFT_ID 2
+#define DRUM_RIGHT_ID 3
 #define SERVO_ID 0
 
 #define EYES 7
@@ -17,8 +19,23 @@ static double gain = 1;
 static maestro_t *maestro;
 static piface_t *piface;
 
+typedef struct {
+    wav_t *servo;
+    audio_meta_t meta_servo;
+    talking_skull_t *talking_skull;
+    unsigned char *servo_data;
+    size_t n_servo_data;
+    servo_operations_t *ops;
+} actor_t;
+
+typedef struct {
+    int id;
+    int up;
+    piface_t *piface;
+} drum_t;
+
 static void
-update_servo(void *unused, double pos)
+update_singer(void *unused, double pos)
 {
     pos *= gain;
     if (pos > 100) pos = 100;
@@ -26,15 +43,47 @@ update_servo(void *unused, double pos)
     piface_set(piface, EYES, pos > 50);
 }
 
+static void
+update_drum(void *drum_as_vp, double pos)
+{
+    drum_t *drum = (drum_t *) drum_as_vp;
+    int this_up = pos > 0.5;
+
+    if (this_up != drum->up) {
+	drum->up = this_up;
+	piface_set(drum->piface, drum->id, drum->up);
+	printf("drum %d @ %d\n", drum->id, drum->up);
+    }
+}
+
+static void
+actor_init(actor_t *a, const char *fname, talking_skull_servo_update_t update, void *data)
+{
+    printf("Loading %s\n", fname);
+    a->servo = wav_new(fname);
+    if (! a->servo) {
+	perror(fname);
+	exit(1);
+    }
+
+    a->meta_servo = wav_get_meta(a->servo);
+    a->servo_data = wav_get_raw_data(a->servo, &a->n_servo_data);
+    a->talking_skull = talking_skull_new(&a->meta_servo, true, update, data);
+    a->ops = talking_skull_prepare(a->talking_skull, a->servo_data, a->n_servo_data);
+}
+
+static void
+actor_play(actor_t *a)
+{
+    talking_skull_play_prepared(a->talking_skull, a->ops);
+}
+
 int
 main(int argc, char **argv)
 {
     track_t *song;
-    wav_t *servo;
-    audio_meta_t meta_servo;
-    talking_skull_t *talking_skull;
-    unsigned char *servo_data;
-    size_t n_servo_data;
+    actor_t singer, a_left, a_right;
+    drum_t left, right;
 
     pi_usb_init();
 
@@ -68,18 +117,23 @@ main(int argc, char **argv)
 	exit(1);
     }
 
-    servo = wav_new("under-the-sea-singer.wav");
-    if (! servo) {
-	perror("under-the-sea-singer.wav");
-	exit(1);
-    }
+    left.id = DRUM_LEFT_ID;
+    left.up = 0;
+    left.piface = piface_new();
+    right.id = DRUM_RIGHT_ID;
+    right.up = 0;
+    right.piface = piface_new();
 
-    meta_servo = wav_get_meta(servo);
-    servo_data = wav_get_raw_data(servo, &n_servo_data);
-    talking_skull = talking_skull_new(&meta_servo, true, update_servo, NULL);
+    actor_init(&singer, "under-the-sea-singer.wav", update_singer, NULL);
+    actor_init(&a_left, "under-the-sea-drum-left.wav", update_drum, &left);
+    actor_init(&a_right, "under-the-sea-drum-right.wav", update_drum, &right);
+
+    printf("Starting!\n");
 
     while (true) {
-	talking_skull_play(talking_skull, servo_data, n_servo_data);
+	actor_play(&singer);
+	actor_play(&a_left);
+	actor_play(&a_right);
 	track_play(song);
 	ms_sleep(INTER_SONG_MS);
     }
---
+++ 2015/band.d
@@ -0,0 +1,5 @@
+band.o: band.c /home/crpalmer/lib/pi-usb.h /home/crpalmer/lib/maestro.h \
+ /home/crpalmer/lib/piface.h /home/crpalmer/lib/util.h \
+ /home/crpalmer/lib/talking-skull.h /home/crpalmer/lib/audio-config.h \
+ /home/crpalmer/lib/track.h /home/crpalmer/lib/wav.h \
+ /home/crpalmer/lib/talking-skull.h /home/crpalmer/lib/audio.h
