913418fdfdc09de10ec642d2e8651cba62808d4a
Christopher R. Palmer <crpalmer@gmail.com>
2016-05-16 15:23:59 -0400

    lightning: Auto run the program based on date & time of day
    On halloween, crank up the volume and run late.

--- lightning.c
+++ lightning.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "track.h"
 #include "util.h"
 #include "wb.h"
@@ -17,6 +18,17 @@ static int bolt_times[N_TRACKS][N_BOLTS] = {
     { 2200,    0,    0,    0,  0 },
     { 1700, 1400,    0,    0,  0 },
 };
+
+#define START_HOUR	16
+#define START_MINUTES   15
+#define END_HOUR	20
+#define END_MINUTES	00
+#define HALLOWEEN_END_HOUR 23
+#define HALLOWEEN_END_MINUTES 00
+#define NORMAL_END	MINUTES(END_HOUR, END_MINUTES)
+#define HALLOWEEN_END	MINUTES(HALLOWEEN_END_HOUR, HALLOWEEN_END_MINUTES)
+
+#define MINUTES(hour, minutes) ((hour)*60 + (minutes))
 
 static void
 load_tracks(void)
@@ -50,6 +62,41 @@ set_flash(wb_t *wb, bool on)
     for (i = 0; i < 16; i++) wb_set(wb, i, on);
 }
 
+#define OCTOBER	9	/* 0 based month number */
+#define WEEN_DAY 31
+
+static bool
+is_halloween_within_days(unsigned days)
+{
+    time_t now = time(NULL);
+    struct tm tm;
+
+    if (localtime_r(&now, &tm)) {
+	return (tm.tm_mon == OCTOBER && (tm.tm_mday >= WEEN_DAY - days && tm.tm_mday <= WEEN_DAY));
+    }
+    return false;
+}
+
+static bool
+is_halloween(void)
+{
+    return is_halloween_within_days(0);
+}
+
+static bool
+is_valid_time_of_day(void)
+{
+    time_t now = time(NULL);
+    struct tm tm;
+
+    if (localtime_r(&now, &tm)) {
+	unsigned min = MINUTES(tm.tm_hour, tm.tm_min);
+	unsigned end = is_halloween() ? HALLOWEEN_END : NORMAL_END;
+	return min >= MINUTES(START_HOUR, START_MINUTES) && min < end;
+    }
+    return true;
+}
+
 int
 main(int argc, char **argv)
 {
@@ -69,12 +116,22 @@ main(int argc, char **argv)
 	int track;
 	int bolt;
 
+	if (! is_valid_time_of_day()) {
+	    printf("Exiting valid time of day, waiting for %d:%02d\n", START_HOUR, START_MINUTES);
+	    while (! is_valid_time_of_day()) {
+		ms_sleep(60*1000);
+	    }
+	    printf("Entering valid time of day at %d:%02d\n", END_HOUR, END_MINUTES);
+	}
+
 	ms_sleep(random_number_in_range(2000, 5000));
 
 	do {
 	    track = random_number_in_range(0, N_TRACKS-1);
 	} while (track == last_track);
 	last_track = track;
+
+	track_set_volume(tracks[track], is_halloween_within_days(1) ? 100 : 75);
 
 	track_play_asynchronously(tracks[track]);
 
